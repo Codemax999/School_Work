0a045f0efb4b930d7e880f80285554c4135b27d7
Cody Maxwell <codemax999@live.com>
2017-11-09 02:14:49 -0500

    MVC Demo Components

---
+++ JS/No_Framework/MVC_No_Events/js/main.js
@@ -0,0 +1,29 @@
+/*
+  Cody Maxwell
+  Design Patterns for Web Programming - Online
+  Assignment 3 - JavaScript MVC
+  11.9.2017
+*/
+
+// on load (instansiates singleton)
+// singleton (loads main program) name: Maxwell_DPW
+// main program (event listeners)
+// data object (structure of the full form object)
+// model (process the data and returns to controller)
+// view (displays the new data)
+// controller (handles input data)
+// utility (some form of calculation)
+
+
+// the page loads and on load 
+// a singleton is created
+// which creates a single main program
+// the main program assigns listeners
+// that eventually uses the controller
+// to create a data object
+// and pass to model
+// which does data manipulation
+// and uses the utility class
+// then returns to the controller
+// where the controller passes it along
+// to the view that displays the data
